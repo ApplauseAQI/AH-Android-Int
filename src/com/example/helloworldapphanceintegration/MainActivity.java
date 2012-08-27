54df3166d0d42b260596fba896008345dafcac97
Wojciech Kedzierski <wkedzierski@utest.com>
2012-08-27 14:39:28 +0200

    Starting Apphance session in the main activity class

--- src/com/example/helloworldapphanceintegration/MainActivity.java
+++ src/com/example/helloworldapphanceintegration/MainActivity.java
@@ -6,11 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
+import com.apphance.android.Apphance;
+
 public class MainActivity extends Activity {
+	
+	public static final String APP_KEY = "6dca8d31314ce8e20ab6df20d6a786cd5147816d";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        Apphance.startNewSession(this, APP_KEY, Apphance.Mode.QA);
+        
         setContentView(R.layout.activity_main);
     }
 
