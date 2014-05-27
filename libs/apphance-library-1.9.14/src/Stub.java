4565a1ff4e377fafa058a088a71db76312e9768d
Przemek Jakubczyk <pjakubczyk@utest.com>
2014-05-27 14:52:36 +0200

    Moving project to gradle based with appplause integration

--- libs/apphance-library-1.9.14/src/Stub.java
+++
@@ -1,5 +0,0 @@
-/*
-This file is intentionally here - DO NOT REMOVE IT!
-It's used to be copied into src/ folder, since Android SDK tools fails when src folder does no exists.
-All classes are distributed as compiled *.class file in attached JAR file
-*/
---
+++ sample/src/main/java/com/applause/hellointegration/MainActivity.java
@@ -0,0 +1,108 @@
+package com.applause.hellointegration;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.applause.android.Applause;
+import com.applause.android.Log;
+import com.applause.android.common.IBuilder;
+import com.applause.android.config.Configuration;
+
+
+/***********************************/
+/* Configure Logging with Applause */
+
+/**
+ * *******************************
+ */
+// The next line will import Applause's built-in logging methods. You should use
+// these in place of Android's regular logging methods. Applause's methods
+// automatically send log messages to the Applause servers where you can review
+// log data along with crash reports and bug reports. In addition, Applause's
+// logging methods will still make your log entries available locally in Logcat.
+// Learn more by visiting
+// https://help.applause.com/hc/en-us/articles/201954883-Android-SDK-Pre-Production-Installation-Guide
+
+public class MainActivity extends Activity {
+
+    /************************************/
+    /* Application Key for for Applause */
+
+    /**
+     * ****************************
+     */
+    // You must insert your Applause application key in the next line of code:
+    public static final String APP_KEY = "Your-Applause-Application-Key-Goes-Here";
+
+    // If you do not have an Applause application key, you must first create a
+    // free account. Visit http://www.applause.com for more information about
+    // how to create your account.
+    //
+    // If you already have an Applause account, login to the Applause control
+    // panel at: https://sdk.applause.com
+    // Create a new application (if you have not done so already). Your
+    // application key can be retrieved at any time from the "Settings" tab
+    // on the left menu.
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        if (APP_KEY.equals("Your-Applause-Application-Key-Goes-Here")) {
+            Toast.makeText(this, "You have not provided Applause Application Key in MainActivity.APP_KEY",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        /********************************/
+        /* Creating session configuration */
+        /********************************/
+        // Here create a configuration Applause will use to start a session.
+        // Configuration object uses builder pattern, so you can chain calls and
+        // make the process of creation more compact.
+        IBuilder builder = new Configuration.Builder(this);
+        builder = builder.withServerURL("https://sdk.applause.com"); // specify
+        // server,
+        // skip
+        // for
+        // default
+        // ("http://panel.Applause.com")
+        builder = builder.withAPIKey(APP_KEY); // specify app key
+        builder = builder.withReportOnShakeEnabled(true); // report on shake
+        // feature is on by
+        // default
+        builder = builder.withUTestEnabled(false); // you can turn features used
+        // by utest emplyoees on,
+        // it's off by default
+//        builder = builder.withDefaultUser("anonymous@apphance.com"); // specify
+        // user to
+        // log in
+        // as, skip
+        // for
+        // manual
+        // selection
+
+        Configuration configuration = builder.build();
+
+        /********************************/
+        /* Starting an Applause session */
+        /********************************/
+        // The following line of code actually starts an Applause session in QA
+        // mode. Learn more by visiting
+        // http://help.Applause.com/library-installation/android/starting-session
+        Applause.startNewSession(this, configuration);
+
+        /*************************/
+        /* Logging with Applause */
+        /*************************/
+        // Applause can replace the default logging mechanism with its own
+        // mechanism, assuming you import com.Applause.android.Log. See above
+        // for more details.
+        Log.w("myTag", "This log message will be sent to Applause.");
+    }
+
+
+}
